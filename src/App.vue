94ad2f158bbd3a469a614bf288b866c6d12dd483
Leandro Frederico <leandro_frederico@hotmail.com>
2021-06-08 16:54:40 -0300

    wip - backend comunications

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <router-view />
 </template>
+
 <script>
 import { mapMutations } from 'vuex'
 import mitt from 'mitt'
 window.mitt = new mitt()
 
 export default {
+  beforeMount() {
+    this.getData()
+  },
   mounted() {
     this.setInitialActivity(new Date())
   },
   methods: {
     ...mapMutations({
       setInitialActivity: 'global/setInitialActivity'
-    })
+    }),
+    async getData() {
+      const ax = this.axios.create({
+        baseURL: 'http://localhost:3000',
+        timeout: 1000,
+        headers: { 'Content-Type': 'application/json' }
+      })
+
+      try {
+        const res = await ax.get('/tags', { data: { qty_per_page: 5, page: 1 } })
+        console.log(res)
+      } catch (err) {
+        console.log(err.response && err.response.data)
+      }
+    }
   }
 }
 </script>
+
 <style lang="scss">
 html,
 body,
@@ -28,9 +47,6 @@ body,
   overflow-x: hidden;
   background: $bg-primary;
   max-width: 100%;
-}
-
-#app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   user-select: none;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import './assets/styles.css'
+
+axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.timeout = 1000
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(VueAxios, axios)
   .mount('#app')
